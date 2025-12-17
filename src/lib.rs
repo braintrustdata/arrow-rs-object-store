66e531220823567aa69fdd1951a5609027eaa6df
Austin Moehle <frozenvapor100@gmail.com>
2025-12-17 13:27:01 -0800

    delete_stream concurrency 10 -> 1000

--- src/lib.rs
+++ src/lib.rs
@@ -724,7 +724,7 @@ pub trait ObjectStore: std::fmt::Display + Send + Sync + Debug + 'static {
                 self.delete(&location).await?;
                 Ok(location)
             })
-            .buffered(10)
+            .buffered(1000)
             .boxed()
     }
 
