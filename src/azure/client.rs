3d66941908f27af3879533de836109eab5425a54
Hurshal Patel <hurshal@braintrustdata.com>
2026-03-25 22:55:14 -0700

    pin action versions and cargo fmt

--- src/azure/client.rs
+++ src/azure/client.rs
@@ -1189,8 +1189,8 @@ pub(crate) struct UserDelegationKey {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::client::mock_server::MockServer;
     use crate::StaticCredentialProvider;
+    use crate::client::mock_server::MockServer;
     use bytes::Bytes;
     use http::Response;
     use regex::bytes::Regex;
@@ -1544,7 +1544,10 @@ Time:2018-06-14T16:46:54.6040685Z</Message></Error>\r
         server.push_fn(move |req| {
             assert_eq!(req.method(), Method::PUT);
             assert_eq!(req.headers().get(IF_NONE_MATCH).unwrap(), "*");
-            Response::builder().status(status).body(String::new()).unwrap()
+            Response::builder()
+                .status(status)
+                .body(String::new())
+                .unwrap()
         });
 
         let credential_provider = Arc::new(StaticCredentialProvider::new(
